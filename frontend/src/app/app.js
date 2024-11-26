26dad30eeb2742b6b3f58deacbac4ec7afd91950
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-11-26 22:39:00 +0700

    add : page login & component navbar

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -1,13 +1,23 @@
+"use client";
+
 import Footer from "@/components/layouts/footer";
 import NavigationBar from "@/components/layouts/navbar";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const App = ({ children }) => {
+  const pathname = usePathname();
+
+  const hiddenPaths = ["/signin"];
+  const shouldHideMenu = hiddenPaths.includes(pathname);
+
   return (
     <React.Fragment>
-      <NavigationBar />
-      <main>{children}</main>
-      <Footer />
+      {!shouldHideMenu && <NavigationBar />}
+      <main className={!shouldHideMenu ? "px-[50px]" : undefined}>
+        {children}
+      </main>
+      {!shouldHideMenu && <Footer />}
     </React.Fragment>
   );
 };
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,4 +1,4 @@
-const Home = () => {
+const Homepage = () => {
   return (
     <section className="h-screen">
       <h2>Welcome To NeoKom</h2>
@@ -6,4 +6,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Homepage;
