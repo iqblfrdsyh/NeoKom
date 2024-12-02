b2c27f1cb93924fe5f618fdd6e8e1a52e1325f77
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-12-02 20:29:52 +0700

    revisi

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,14 +1,9 @@
-import CourseSection from "@/components/sections/home/courseSection/courseSection";
-import HeroSection from "@/components/sections/home/heroSection/herosection";
-import QuizSection from "@/components/sections/home/quizSection/quizSection";
 import React from "react";
 
 const Homepage = () => {
   return (
     <React.Fragment>
-      <HeroSection />
-      <CourseSection />
-      <QuizSection />
+      <h2>Test</h2>
     </React.Fragment>
   );
 };
