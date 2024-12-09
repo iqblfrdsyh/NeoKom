42bf5245156414ed3e3bcfe0687a425a810a738f
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-12-09 19:51:29 +0700

    update func hooks

--- frontend/src/hooks/useAuthentication.js
+++ frontend/src/hooks/useAuthentication.js
@@ -1,48 +1,54 @@
 import { authenticate } from "@/lib/authenticate";
 import { getToken } from "@/lib/utils";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useCallback, useEffect } from "react";
 
 function useAuthentication() {
   const pathname = usePathname();
+  const router = useRouter();
 
   const checkAuth = useCallback(async (token) => {
     try {
       const response = await authenticate("protected", token);
 
       if (response.status === 401) {
-        window.location.href = "/signin";
+        const redirectPath = pathname.includes("admin") ? "/admin/signin" : "/signin";
+        router.replace(redirectPath);
         return;
       }
 
-      const { role } = response.data.data;
+      const { role } = response?.data?.data;
 
       if (pathname.includes("/admin")) {
         if (role !== "teacher") {
-          console.log("Illegal access!");
-          window.location.href = "/admin/signin";
+          console.warn("Illegal access!");
+          router.replace("/admin/signin");
+        } else if (pathname === "/admin/signin") {
+          router.replace("/admin/dashboard");
         }
       } else if (role !== "student") {
-        console.log("Illegal access!");
-        window.location.href = "/signin";
+        console.warn("Illegal access!");
+        router.replace("/signin");
+      } else if (pathname === "/signin") {
+        router.replace("/");
       }
     } catch (error) {
       console.error("Authentication failed:", error.message);
-      window.location.href = "/signin";
+      const redirectPath = pathname.includes("admin") ? "/admin/signin" : "/signin";
+      router.replace(redirectPath);
     }
-  }, [pathname]);
+  }, [pathname, router]);
 
   useEffect(() => {
-    if (pathname === "/signin" || pathname === "/admin/signin") return;
-
     const token = getToken();
     if (token) {
       checkAuth(token);
     } else {
       console.warn("No token found, redirecting to /signin");
-      window.location.href = "/signin";
+      const redirectPath = pathname.includes("admin") ? "/admin/signin" : "/signin";
+      router.replace(redirectPath);
     }
-  }, [pathname, checkAuth]);
+  }, [pathname, checkAuth, router]);
 
   return null;
 }
